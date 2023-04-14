1406e5315dc1d9ce91ec05455787b25f69564205
Marius Sørensen <marius.sorensen@miles.no>
2023-04-14 15:23:08 +0200

    Here we go again

---
+++ KolumbusToRide/Domain/PlayerState.cs
@@ -0,0 +1,15 @@
+﻿using DeckOfCards;
+using Kolumbus;
+
+namespace KolumbusToRide.Domain;
+
+public class PlayerState
+{
+    private string Name { get; set; }
+    private int Score { get; set; }
+    private TimeSpan TimeUsed { get; set; }
+    private TimeSpan TimeLeft { get; set; }
+    private List<Card> Hand { get; set; }
+    private StopPlace CurrentPosition { get; set; }
+    private List<Vehicle> PossibleTransportations { get; set; }
+}
--- KolumbusToRide/Program.cs
+++ KolumbusToRide/Program.cs
@@ -1,6 +1,16 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// ----- Endpoints -------------------------------------------------------
 app.MapGet("/", () => "Hello World!");
+
 app.Run();
