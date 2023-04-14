19994ab37154e4bd728868e4c1a74dd90c2d2d7f
Marius Sørensen <marius.sorensen@miles.no>
2023-04-14 15:47:45 +0200

    Get my boi

---
+++ KolumbusToRide/Dto/GetOnVehicleDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Kolumbus2Ride.Dto;
+
+public class GetOnVehicleDto
+{
+    public string VehicleId { get; set; }
+}
---
+++ KolumbusToRide/Services/GameService.cs
@@ -0,0 +1,20 @@
+﻿using Kolumbus;
+using KolumbusToRide.Domain;
+
+namespace Kolumbus2Ride.Services;
+
+public static class GameService
+{
+    public static PlayerState GetOnVehicle(PlayerState playerState, string vehicleId)
+    {
+        // 1. Get next stop
+        // 2. Add time to next stop to playerState.timePlayed
+        // 3. Set position to next stop
+        // 4. Use relevant cards
+        // 5. Update score - check finished goal route
+        // 6. Check if more time left
+        playerState.Name = "BRYNJ";
+
+        return playerState;
+    }
+}
